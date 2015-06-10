7aa2438f32aae35eadf73086cfec4b0f3edb06c6
Suraj Sinha <ssinha10@illinois.edu>
2015-06-10 18:39:24 -0500

    Initial Commit

---
+++ CoreMovement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMovement
+//
+//  Created by Suraj Sinha on 6/10/15.
+//  Copyright (c) 2015 KingSoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
